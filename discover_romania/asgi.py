dba8a05783ce1a36547369214686ff431ff4eb76
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-04 09:44:02 +0000

    Initial Commit

---
+++ discover_romania/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for discover_romania project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discover_romania.settings')
+
+application = get_asgi_application()
---
+++ discover_romania/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for discover_romania project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discover_romania.settings')
+
+application = get_wsgi_application()
