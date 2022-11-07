b382fb84d610108db8b74e93b1dfa707a75394cc
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-07 16:50:14 +0000

    deployed to heroku

--- discover_romania/settings.py
+++ discover_romania/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
