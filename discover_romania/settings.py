e8b2db37811094ca14713b6006387fdff490fbca
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-07 19:06:46 +0000

    deployment static file

--- discover_romania/settings.py
+++ discover_romania/settings.py
@@ -171,7 +171,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
