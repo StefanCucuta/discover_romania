780513666705e5c8de0fc6f8c3a077eab9c7bd48
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-07 14:50:35 +0000

    Added another app

--- discover_romania/settings.py
+++ discover_romania/settings.py
@@ -106,19 +106,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'discover_romania.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+Database
+https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 
 
 # Password validation
