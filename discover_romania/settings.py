28365efd21878f198a7a6a29c713327a0e59d73a
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-07 17:03:00 +0000

    debug false

--- discover_romania/settings.py
+++ discover_romania/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
