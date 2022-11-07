14e8cc4cf9475638844af5e0179716f066c49133
Stefan Cucuta <cucutastefan@gmail.com>
2022-11-07 16:45:17 +0000

    favicon error

--- discover_romania/settings.py
+++ discover_romania/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
